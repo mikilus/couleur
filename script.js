dc220b9313d262bb9517df51d2b5e188a425ad8b
Kyllian BANCHEREAU <kyllian.banchereau@etu.univ-nantes.fr>
2020-02-11 12:07:41 +0100

    correction

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-
-let tabCouleur = ['red', 'yellow', 'green', 'purple', 'black', 'blue', 'pink'];
-
 let changer = function(val){
-    document.getElementsByTagName('body')[0].style.backgroundColor= tabCouleur[getRandomInt(7)];
+	document.getElementsByTagName('body')[0].style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 }
 let timer = null;
 
-
-function stop() {
-   clearInterval(timer);
-   timer = null;
-
+function stop(){
+	clearInterval(timer);
+	timer = null;
 };
+
+
 function lancer() {
-  if (timer !=null) {return};
-  timer =setInterval(() => {changer(true);}, 300);
+	if(timer != null){
+		return;
+	}
+	timer = setInterval(() => {changer(true);},300);
 };
